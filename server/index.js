5b9ac93a77e52ea9659b98d71dc851bf9918c7ed
ahmedmohamed17 <ahmedmohamed7sayed@gmail.com>
2024-03-18 22:32:03 +0200

    first change in client && server :fire:

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
-import express from "express.js";
+import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
