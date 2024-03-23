724248ed27f88858e421b3d2601b373827267398
ahmedmohamed17 <ahmedmohamed7sayed@gmail.com>
2024-03-23 21:18:14 +0200

    fix issues in client && working with api

--- client/src/components/Posts/posts.js
+++ client/src/components/Posts/posts.js
@@ -1,7 +1,10 @@
 import React from "react";
 import Post from "./Post/Post.js";
+import useStyles from "./styles.js";
 
 const Posts = () => {
+  // const classes = useStyles();
+
   return (
     <React.Fragment>
       <h1>Posts</h1>
