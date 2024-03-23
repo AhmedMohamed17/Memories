b089a7e1cb4a9fcf98384e5964454dc7e9cf8ec1
ahmedmohamed17 <ahmedmohamed7sayed@gmail.com>
2024-03-23 23:57:12 +0200

    creating main backend

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,11 @@
 import express from "express";
-import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import PostRoutes from "./routes/posts.js";
 
 const app = express();
-const port = process.env.PORT || 5000;
-app.use("/posts", PostRoutes);
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+const port = 3000;
 app.use(cors());
+
 // const CONNECTION_URL = "mongodb+srv://ahmedmohamed7sayed:<password>@memoriesdatabase.g1bd97q.mongodb.net/?retryWrites=true&w=majority&appName=MemoriesDataBase"
 const CONNECTION_URL = "mongodb://127.0.0.1:27017/MEMOREIES-DEMO";
 
