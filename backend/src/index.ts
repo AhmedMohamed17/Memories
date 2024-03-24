5659d416f4012d5d3450dc8fcea5c5161fc9a775
ahmedmohamed17 <ahmedmohamed7sayed@gmail.com>
2024-03-25 01:26:33 +0200

    added some functions in controller and routes :eyes:

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express ,{ Request ,Response} from "express";
 import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose'
+import myUserRoute from './routes/MyUserRoutes'
 const PORT = 3000 ;
 mongoose
 .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -11,9 +12,8 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 
-app.get("/test" , async (req:Request ,res:Response)=>[
-res.json({message:"Hello!"})
-]);
+app.use("/api/my/user" , myUserRoute )
+
 
 app.listen(PORT ,()=>{
     console.log(`server started on localhost:${PORT}`)
