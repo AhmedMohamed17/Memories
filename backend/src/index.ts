70b243f71502080161ac03173b184fbffab112a9
ahmedmohamed17 <ahmedmohamed7sayed@gmail.com>
2024-03-24 19:41:38 +0200

    change port number && make navbar

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express ,{ Request ,Response} from "express";
 import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose'
-
+const PORT = 3000 ;
 mongoose
 .connect(process.env.MONGODB_CONNECTION_STRING as string)
 .then(()=>console.log("Connected to database!"))
@@ -15,6 +15,6 @@ app.get("/test" , async (req:Request ,res:Response)=>[
 res.json({message:"Hello!"})
 ]);
 
-app.listen(7000 ,()=>{
-    console.log("server started on localhost:7000")
+app.listen(PORT ,()=>{
+    console.log(`server started on localhost:${PORT}`)
 })
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import MobileNav from "./MobileNAv";
+import MainNav from "./MainNav";
 
 const Header = () => {
     return (  
@@ -9,6 +11,12 @@ const Header = () => {
                 <Link to="/" className="text-3xl font-bold tracking-tight text-orange-500">
                     MernEats.com
                 </Link>
+                <div className="md:hidden">
+                    <MobileNav/>
+                </div>
+                <div className="hidden md:block">
+                    <MainNav/>
+                </div>
             </div>
         </div>
     );
